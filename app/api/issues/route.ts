aebc88f9735f7bab6423c2fa54e800601cc9313c
unknown <malikhax3@gmail.com>
2023-10-24 12:13:48 +0500

    update issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "../../validationSchemas";
+import { issueSchema } from "../../validationSchemas";
 
 export async function POST(request:NextRequest){
     const body = await request.json();
-    const validate = createIssueSchema.safeParse(body)
+    const validate = issueSchema.safeParse(body)
     if(!validate.success){
         return NextResponse.json(validate.error.format(),{status:400})
     }
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import SimpleMDE from 'react-simplemde-editor';
+import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
@@ -9,44 +9,56 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import { z } from "zod"
+import { issueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 import Spinner from "@/components/ui/Spinner";
 import { Issue } from "@prisma/client";
 import dynamic from "next/dynamic";
 
 // const SimpleMDE = dynamic(()=> import('react-simplemde-editor'),{ssr:false})
-type IssueFormData = z.infer<typeof createIssueSchema>
-const IssueForm = ({ issue }: {issue?:Issue}) => {
+type IssueFormData = z.infer<typeof issueSchema>;
+const IssueForm = ({ issue }: { issue?: Issue }) => {
   const [error, setError] = useState("");
-  const [isSubmitting, setSubmitting] = useState(false)
+  const [isSubmitting, setSubmitting] = useState(false);
   const router = useRouter();
-  const { register, control, handleSubmit, formState:{ errors } } = useForm<IssueFormData>({
-    resolver:zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(issueSchema),
   });
   return (
     <div className="max-w-xl">
-      {error && (<Alert className="mb-4 bg-red-50">
-        {/* <Terminal className="h-4 w-4" /> */}
-        <AlertTitle className="text-red-600">ERROR!!!!</AlertTitle>
-        <AlertDescription>
-          {error}
-        </AlertDescription>
-      </Alert>)}
+      {error && (
+        <Alert className="mb-4 bg-red-50">
+          {/* <Terminal className="h-4 w-4" /> */}
+          <AlertTitle className="text-red-600">ERROR!!!!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
       <form
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
             setSubmitting(true);
-            await axios.post("/api/issues", data);
+            if (issue) 
+              await axios.patch("/api/issues/" + issue.id, data);
+            else 
+              await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
-            setSubmitting(false)
+            setSubmitting(false);
             setError("An Unexpected Error occured.");
           }
         })}
       >
-        <Input placeholder="Title" defaultValue={issue?.title} {...register("title")} />
+        <Input
+          placeholder="Title"
+          defaultValue={issue?.title}
+          {...register("title")}
+        />
         {errors.title && <p className="text-red-600">{errors.title.message}</p>}
         <Controller
           name="description"
@@ -56,9 +68,14 @@ const IssueForm = ({ issue }: {issue?:Issue}) => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && <p className="text-red-600">{errors.description.message}</p>}
-        <Button disabled={isSubmitting} className="bg-violet-600 hover:bg-violet-800">
-          Submit New Issue {isSubmitting && <Spinner />}
+        {errors.description && (
+          <p className="text-red-600">{errors.description.message}</p>
+        )}
+        <Button
+          disabled={isSubmitting}
+          className="bg-violet-600 hover:bg-violet-800"
+        >{issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+           {isSubmitting && <Spinner />} 
         </Button>
       </form>
     </div>
